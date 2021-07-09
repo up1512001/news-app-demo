e5a29c22c4f4b02b3ac95ea163b7f8aacb825e42
up1512001 <utsav.patel1215@outlook.com>
2021-07-09 22:40:35 +0530

    first commit

---
+++ app/src/main/java/com/example/android/newsapp/NewsListAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.android.newsapp
+
+import android.view.LayoutInflater
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Response
+
+class NewsListAdapter(private val listener:NewsItemClicked): RecyclerView.Adapter<NewsViewHolder>() {
+
+    private val items:ArrayList<News> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent,false)
+        val viewHolder = NewsViewHolder(view)
+        view.setOnClickListener{
+            listener.onItemClicked(items[viewHolder.adapterPosition])
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.titleView.text = currentItem.title
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    fun updateNews(updatedNews:ArrayList<News>){
+        items.clear()
+        items.addAll(updatedNews)
+
+        notifyDataSetChanged()
+
+    }
+
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val titleView:TextView = itemView.findViewById(R.id.title)
+}
+
+interface NewsItemClicked{
+    fun onItemClicked(item : News)
+}
